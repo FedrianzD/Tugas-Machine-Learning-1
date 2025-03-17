f8f0332aea85fabd3e88a0b51ada6bfd125a6832
AureliusJustin <88382259+AureliusJustin@users.noreply.github.com>
2025-03-17 08:56:09 +0700

    chore: testcases for autodiff

---
+++ test.py
@@ -0,0 +1,74 @@
+# BUAT NGETES AUTODIFF
+
+import torch
+from AutoDiff import Value
+
+def test_sanity_check():
+
+    x = Value(-4.0)
+    z = 2 * x + 2 + x
+    q = z.relu() + z * x
+    h = (z * z).relu()
+    y = h + q + q * x
+    y.updateGradients()
+    xmg, ymg = x, y
+
+    x = torch.Tensor([-4.0]).double()
+    x.requires_grad = True
+    z = 2 * x + 2 + x
+    q = z.relu() + z * x
+    h = (z * z).relu()
+    y = h + q + q * x
+    y.backward()
+    xpt, ypt = x, y
+
+    print(ymg.value, ypt.data.item())
+    print(xmg.gradient, xpt.grad.item())
+    # forward pass went well
+    assert ymg.value == ypt.data.item()
+    # updateGradients pass went well
+    assert xmg.gradient == xpt.grad.item()
+
+def test_more_ops():
+
+    a = Value(-4.0)
+    b = Value(2.0)
+    c = a + b
+    d = a * b + b**3
+    c += c + 1
+    c += 1 + c + (-a)
+    d += d * 2 + (b + a).relu()
+    d += 3 * d + (b - a).relu()
+    e = c - d
+    f = e**2
+    g = f / 2.0
+    g += 10.0 / f
+    g.updateGradients()
+    amg, bmg, gmg = a, b, g
+
+    a = torch.Tensor([-4.0]).double()
+    b = torch.Tensor([2.0]).double()
+    a.requires_grad = True
+    b.requires_grad = True
+    c = a + b
+    d = a * b + b**3
+    c = c + c + 1
+    c = c + 1 + c + (-a)
+    d = d + d * 2 + (b + a).relu()
+    d = d + 3 * d + (b - a).relu()
+    e = c - d
+    f = e**2
+    g = f / 2.0
+    g = g + 10.0 / f
+    g.backward()
+    apt, bpt, gpt = a, b, g
+
+    tol = 1e-6
+    # forward pass went well
+    assert abs(gmg.value - gpt.data.item()) < tol
+    # updateGradients pass went well
+    assert abs(amg.gradient - apt.grad.item()) < tol
+    assert abs(bmg.gradient - bpt.grad.item()) < tol
+    
+test_sanity_check()
+test_more_ops()
