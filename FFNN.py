cc1987daee6b065a30b72acb12db7720f97d6fdb
AureliusJustin <88382259+AureliusJustin@users.noreply.github.com>
2025-03-17 08:55:22 +0700

    feat: implement model (forward & back propagation)

---
+++ FFNN.py
@@ -0,0 +1,131 @@
+import random
+from AutoDiff import Value
+
+class Neuron():
+    def __init__(self, n_input, activation_function="linear"):
+        self.weight = [Value(random.uniform(-1,1)) for _ in range(n_input)]
+        self.bias = Value(0)
+        self.activation_function = activation_function
+
+    def __call__(self, x):
+        if isinstance(x[0], list):  # Batch input
+            return [self(single_x) for single_x in x]
+        
+        c = sum((wi * xi for wi, xi in zip(self.weight, x)), self.bias)
+        
+        if (self.activation_function=="linear"):
+            return c
+        elif (self.activation_function=="relu"):
+            return c.RELU()
+        # else:
+            # for now only linear & relu
+
+    def parameters(self):
+        return self.weight + [self.bias]
+    
+    def reset_gradient(self):
+        for p in self.parameters():
+            p.gradient = 0
+
+    # def __repr__(self):
+
+class Layer():
+    def __init__(self, n_input, n_output, **kwargs):
+        self.neurons = [Neuron(n_input, **kwargs) for _ in range(n_output)]
+
+    def __call__(self, x):
+        if isinstance(x[0], list):  # Batch input
+            return [[n(single_x) for n in self.neurons] for single_x in x]
+        
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+    
+    def reset_gradient(self):
+        for p in self.parameters():
+            p.gradient = 0
+
+    # def __repr__(self):
+
+class FFNN():
+    def __init__(self, layers, activation_functions, loss_function, weight_initializer, batch_size, learning_rate, epoch, verbose):
+        self.layers = [Layer(layers[i], layers[i+1], activation_function=activation_functions[i]) for i in range(len(layers)-1)]
+        self.loss_function = loss_function
+        self.weight_initializer = weight_initializer
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.epoch = epoch
+        self.verbose = verbose
+
+    def __call__(self, x):
+        if isinstance(x[0], list):
+            for layer in self.layers:
+                x = [layer(sample) for sample in x]
+            return x
+        
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+    
+    def reset_gradient(self):
+        for p in self.parameters():
+            p.gradient = 0
+    
+    def visualize_graph(self):
+        return
+    
+    def visualize_weight_distribution(self, layers):
+        return
+    
+    def visualize_weight_gradient_distibution(self, layers):
+        return
+    
+    def fit(self, X, y):
+        # # Check that X and y have correct shape, set n_features_in_, etc.
+        # X, y = validate_data(self, X, y)
+        # # Store the classes seen during fit
+        # self.classes_ = unique_labels(y)
+
+        # self.X_ = X
+        # self.y_ = y
+        # # Return the classifier
+        # return self
+        
+        if batch_size is None:
+            Xb, yb = X, y
+        else:
+            ri = np.random.permutation(X.shape[0])[:batch_size]
+            Xb, yb = X[ri], y[ri]
+        inputs = [list(map(Value, xrow)) for xrow in Xb]
+        
+        outputs = list(map(model, inputs))
+        losses = [(yi - scorei)**2 for yi, scorei in zip(yb, scores)]
+        loss = sum(losses)/len(losses)
+        
+        for k in range(epoch):
+            self.reset_gradient()
+            loss.updateGradients()
+            for p in self.parameters():
+                p.value -= p.gradient * learning_rate
+                
+            if k%verbose==0:
+                print(f'Epoch {k}, Loss: {loss.value}')
+        return
+
+    def predict():
+        return
+    
+    def save():
+        return
+    
+    def load():
+        return
+    
+    # def __repr__(self):
+        
+        
